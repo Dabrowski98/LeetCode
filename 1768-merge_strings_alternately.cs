b46f35b7dd27c63ced91d30cab47876478b7f3eb
Dabrowski98 <dabrowski.konrad.it@gmail.com>
2024-10-28 10:40:28 +0100

    1768-merge_strings_alternately solved

---
+++ 1768-merge_strings_alternately.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class MergeStringsAlternately
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            var result = new StringBuilder();
+            int i = 0;
+            int lowerLength = Math.Min(word1.Length, word2.Length);
+
+            for (; i < lowerLength; i++)
+            {
+                result.Append(word1[i]);
+                result.Append(word2[i]);
+            }
+
+            return (word1.Length > word2.Length) ? result.Append(word1.Substring(i, word1.Length - i)).ToString() : result.Append(word2.Substring(i, word2.Length - i)).ToString();
+        }
+    }
+}
