f1e35da4f3a9431c31ca0e201afa464446be7bd4
Dabrowski98 <dabrowski.konrad.it@gmail.com>
2024-10-28 10:39:12 +0100

    7-reverse_integer refactor

--- 7-reverse_integer.cs
+++ 7-reverse_integer.cs
@@ -4,27 +4,22 @@ namespace LeetCode
 {
     public class ReverseIntegerClass
     {
-        public class Solution
+        public int Reverse(int x)
         {
-            public int Reverse(int x)
+            var sb = new StringBuilder(x.ToString());
+            char temp;
+            int startIndex = (sb[0] == '-' ? 1 : 0);
+
+            for (int i = 0; i < (sb.Length - startIndex) / 2; i++)
             {
-                var sb = new StringBuilder(x.ToString());
-                char temp;
-                int startIndex = (sb[0] == '-' ? 1 : 0);
-
-                for (int i = 0; i < (sb.Length - startIndex) / 2; i++)
-                {
-                    temp = sb[sb.Length - 1 - i];
-                    sb[sb.Length - 1 - i] = sb[i + startIndex];
-                    sb[i + startIndex] = temp;
-                }
-
-                int result = (Int32.TryParse(sb.ToString(), out result)) ? result : 0;
-
-                return result;
+                temp = sb[sb.Length - 1 - i];
+                sb[sb.Length - 1 - i] = sb[i + startIndex];
+                sb[i + startIndex] = temp;
             }
+
+            int result = (Int32.TryParse(sb.ToString(), out result)) ? result : 0;
+
+            return result;
         }
-
     }
-
 }
