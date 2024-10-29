7f8a462183217b20b00b35407dd6d11d63dc15bd
Dabrowski98 <dabrowski.konrad.it@gmail.com>
2024-10-29 23:28:48 +0100

    1431-kids_with_the_greatest_number_of_candies solved

---
+++ 1431-kids_with_the_greatest_number_of_candies.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode
+{
+    public class KidsWithTheGreatestNumberOfCandiesClass
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var result = new List<bool>();
+            byte maxCandies = 0;
+
+            foreach (var cand in candies)
+                if (cand > maxCandies) maxCandies = (byte)cand;
+
+            foreach (var cand in candies)
+                result.Add(cand + extraCandies >= maxCandies ? true : false);
+
+            return result;
+        }
+    }
+}
