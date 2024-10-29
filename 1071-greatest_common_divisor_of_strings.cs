a9b8ddf4c035c0a671ff5f6dd65c57267f4d82f0
Dabrowski98 <dabrowski.konrad.it@gmail.com>
2024-10-29 22:52:41 +0100

    1071-greatest_common_divisor_of_strings solved

---
+++ 1071-greatest_common_divisor_of_strings.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode
+{
+    public class GreatestCommonDivisorOfStringsClass
+    {
+        public string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1) return "";
+
+            int a = str1.Length;
+            int b = str2.Length;
+            int temp;
+
+            while (b > 0)
+            {
+                temp = a;
+                a = b;
+                b = temp % b;
+            }
+            return str2.Substring(0, a);
+        }
+    }
+}
